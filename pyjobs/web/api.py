43143b70d72a568226d76d36b03b401c21e6fecd
hemalvarambhia <h.n.varambhia@gmail.com>
2023-07-31 20:58:28 +0100

    Added more endpoints.

--- pyjobs/web/api.py
+++ pyjobs/web/api.py
@@ -26,3 +26,58 @@ def post_job():
 @router.get("/jobs/{id}")
 def get_job():
     pass
+
+
+@router.put("/jobs/{id}")
+def update_job():
+    pass
+
+
+@router.post("/jobs/{id}/application")
+def apply_for_job():
+    pass
+
+
+@router.get("/jobs/{id}/applications")
+def list_job_applications_recruiter():
+    pass
+
+
+@router.get("/applications")
+def list_applications_made_by_candidate():
+    pass
+
+
+@router.get("/applications/{id}")
+def get_applications_made_by_candidate():
+    pass
+
+
+@router.post("/applications/{id}/cancel")
+def cancel_an_application():
+    pass
+
+
+@router.get("/candidates")
+def list_candidates():
+    pass
+
+
+@router.post("/candidates")
+def register_candidate():
+    pass
+
+
+@router.get("/candidates/{id}")
+def get_candidate():
+    pass
+
+
+@router.put("/candidates/{id}")
+def update_candidate():
+    pass
+
+
+@router.delete("/candidates/{id}")
+def delete_candidate():
+    pass
